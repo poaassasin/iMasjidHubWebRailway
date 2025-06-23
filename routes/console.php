5c4b23e8030d64450d08c8fd2df8e7636f4cee82
poaassasin <109660255+poaassasin@users.noreply.github.com>
2025-06-23 10:09:32 +0700

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('jadwal:fetch')->dailyAt('01:00');
+Schedule::command('jadwal:fetch')->everyFiveMinutes();
